9d5c06790453aa743c94a766b60fbef295e78177
Lê Quang Phước <75266051+LeQuangPhuoc2002@users.noreply.github.com>
2024-09-24 19:20:41 +0700

    Update course.route.js

--- app/routes/course.route.js
+++ app/routes/course.route.js
@@ -2,15 +2,6 @@ module.exports = app => {
     const courseController = require("../controllers/course.controller");
     const courseRouter = require("express").Router();
 
-    // Route to retrieve all todos
-    // courseRouter.get("/playlist", courseController.findAll);
-    // // courseRouter.get("/playlistbuy", courseController.findAllplaylistbuy);
-    // // courseRouter.get("/playlist_role", courseController.findAllplaylistbuy);
-    // // courseRouter.get("playlist/create", comment_playlist.create);
-    // courseRouter.get("/watch-video", courseController.findvideo);
-    // courseRouter.get("/courses", courseController.findallcourse);
-    // courseRouter.get("/question", courseController.allquesstion);
-
     courseRouter.get("/course-list", courseController.findAllCourse);
     courseRouter.get("/course-video-list", courseController.findCouseVideoList);
     courseRouter.get("/course-quiz-list", courseController.findCouseQuizList);
@@ -31,17 +22,13 @@ module.exports = app => {
     // courseRouter.get("/ordersuccess", courseController.addcouresetouser);
 
     // courseRouter.get("/chat_roomm", courseController.findallroombyemail);
-    // Attach adminRouter to '/admin' path
     // app.use("/courses", courseRouter);
 
-     // Retrieve a single todo with id
     //  teacherRouter.get("/:name", teacherController.findteacherwithname);
-    //  // Update a todo with id
     //  router.put("/:id", todo.update);
 
     app.use("/", courseRouter);
 
-    // Error handling routes
     app.get('/500', (req, res) => {
         res.render('err');
     });
