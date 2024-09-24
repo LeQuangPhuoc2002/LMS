ac7b4c0a3bf33257a74fb7d978de4ae3bf90bb1a
Lê Quang Phước <75266051+LeQuangPhuoc2002@users.noreply.github.com>
2024-09-24 19:28:19 +0700

    Update user.route.js

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -2,30 +2,9 @@ module.exports = app => {
     const userController = require("../controllers/user.controller");
     const userRouter = require("express").Router();
 
-    // Route to retrieve all todos
     userRouter.get("/student-list", userController.findAllStudent);
-
-    // userRouter.get("/teacher-list-un", userController.findAllTeacherUn);
     userRouter.get("/teacher-un-detail", userController.findAllTeacherUnDetail);
-    // // courseRouter.get("/playlistbuy", courseController.findAllplaylistbuy);
-    // courseRouter.get("/playlist_role", courseController.findAllplaylistbuy);
-    // // courseRouter.get("playlist/create", comment_playlist.create);
-    // courseRouter.get("/watch-video", courseController.findvideo);
-    // courseRouter.get("/courses", courseController.findallcourse);
-    // courseRouter.get("/indexusers", courseController.findallcourseindex);
-    // courseRouter.get("/profile", courseController.findallcoursebyemail);
-    // courseRouter.get("/ordersuccess", courseController.addcouresetouser);
-
-    // Attach adminRouter to '/admin' path
-    // app.use("/courses", courseRouter);
-
-     // Retrieve a single todo with id
-    //  teacherRouter.get("/:name", teacherController.findteacherwithname);
-    //  // Update a todo with id
-    //  router.put("/:id", todo.update);
-
     app.use("/", userRouter);
-    // Error handling routes
     app.get('/500', (req, res) => {
         res.render('err');
     });
